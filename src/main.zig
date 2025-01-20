fbba222f5b0c494474ec80e71e1dcc6b762fce6e
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-01-20 01:35:29 -0500

    vewy nice

--- src/main.zig
+++ src/main.zig
@@ -46,12 +46,14 @@ const Termio = struct {
 const Editor = struct {
     var done: bool = false;
 
-    var cols: u16 = undefined;
-    var rows: u16 = undefined;
-    var cy: u16 = 0; // cursor row
-    var cx: u16 = 0; // cursor col
-    var ro: u16 = 0; // window first row
-    var co: u16 = 0; // window first col
+    var cols: u32 = undefined; // editor rows
+    var rows: u32 = undefined; // editor columns
+
+    var cy: u32 = 0; // cursor row
+    var cx: u32 = 0; // cursor col
+
+    var rowoff: u32 = 0; // window first row
+    var coloff: u32 = 0; // window first col
 
     var alloc = std.heap.GeneralPurposeAllocator(.{}){};
     var data = std.ArrayList(std.ArrayList(u8)).init(alloc.allocator());
@@ -76,10 +78,14 @@ const Editor = struct {
                 Escape.SHOW_CURSOR => try writer.writeAll("\x1b[?25h"),
                 Escape.CLEAR_SCREEN => try writer.writeAll("\x1b[2J"),
                 Escape.CURSOR_TL => try writer.writeAll("\x1b[H"),
-                Escape.MOVE_CURSOR => try std.fmt.format(writer, "\x1b[{d};{d}H", .{ cy + 1, cx + 1 }),
+                Escape.MOVE_CURSOR => try std.fmt.format(writer, "\x1b[{d};{d}H", .{ cy - rowoff + 1, get_cx() - coloff + 1 }),
             }
         }
     };
+
+    fn get_cx() u32 {
+        return @min(cx, data.items[cy].items.len);
+    }
 
     fn init() !void {
         var wsz: posix.system.winsize = undefined;
@@ -91,6 +97,8 @@ const Editor = struct {
 
         cols = wsz.ws_col;
         rows = wsz.ws_row;
+
+        try data.append(std.ArrayList(u8).init(alloc.allocator()));
     }
 
     fn render() !void {
@@ -104,19 +112,18 @@ const Editor = struct {
 
         var row: usize = 0;
         while (row < rows - 1) : (row += 1) {
-            if (row + ro < data.items.len) {
-                var len = data.items[row + ro].items.len - co;
+            if (row + rowoff < data.items.len) {
+                var len = data.items[row + rowoff].items.len - coloff;
                 if (len > cols) len = cols;
 
-                try writer.writeAll(data.items[row + ro].items[co .. co + len]);
+                try writer.writeAll(data.items[row + rowoff].items[coloff .. coloff + len]);
             } else {
                 try writer.writeAll("~");
             }
             try Escape.CLEAR_LINE.write(writer);
             try writer.writeAll("\r\n");
         }
-
-        try writer.writeAll(cmd.items[0..]);
+        try writer.writeAll(cmd.items);
 
         try Escape.MOVE_CURSOR.write(writer);
         try Escape.SHOW_CURSOR.write(writer);
@@ -133,30 +140,7 @@ const Editor = struct {
             Mode.Insert => try handle_insert(ch),
             Mode.Command => try handle_command(ch),
         }
-
-        if (cy + ro < data.items.len) {
-            const width: u16 = @intCast(data.items[cy + ro].items.len);
-            if (cx + co > width) {
-                cx = @min(cols - 1, width);
-                co = width - cx;
-            }
-        } else {
-            if (cy + ro != 0) {
-                cy -= 1;
-            } else {
-                cx = 0;
-                co = 0;
-            }
-        }
-
-        if (cx == cols) {
-            cx -= 1;
-            co += 1;
-        }
-        if (cy == rows) {
-            cy -= 1;
-            ro += 1;
-        }
+        scroll();
     }
 
     fn handle_normal(ch: u8) !void {
@@ -166,29 +150,7 @@ const Editor = struct {
             },
             'i' => {
                 mode = Mode.Insert;
-                if (data.items.len == 0) {
-                    try data.append(std.ArrayList(u8).init(alloc.allocator()));
-                }
-            },
-            'h' => {
-                if (cx > 0) {
-                    cx -= 1;
-                } else if (co > 0) {
-                    co -= 1;
-                }
-            },
-            'j' => {
-                cy += 1;
-            },
-            'k' => {
-                if (cy > 0) {
-                    cy -= 1;
-                } else if (ro > 0) {
-                    ro -= 1;
-                }
-            },
-            'l' => {
-                cx += 1;
+                cx = get_cx();
             },
             else => {},
         }
@@ -200,26 +162,21 @@ const Editor = struct {
                 mode = Mode.Normal;
             },
             '\r' => {
-                try data.insert(ro + cy + 1, std.ArrayList(u8).init(alloc.allocator()));
-                const writer = data.items[ro + cy + 1].writer();
-                _ = try writer.write(data.items[ro + cy].items[cx + co ..]);
-                try data.items[ro + cy].resize(cx + co);
+                try data.insert(cy + 1, std.ArrayList(u8).init(alloc.allocator()));
+                const writer = data.items[cy + 1].writer();
+                _ = try writer.write(data.items[cy].items[cx..]);
+                try data.items[cy].resize(cx);
                 cy += 1;
                 cx = 0;
-                co = 0;
             },
             '\x7f' => {
-                if (cx + co == 0) {} else {
-                    _ = data.items[cy + ro].orderedRemove(cx + co - 1);
-                    if (cx > 0) {
-                        cx -= 1;
-                    } else {
-                        co -= 1;
-                    }
+                if (cx == 0) {} else {
+                    _ = data.items[cy].orderedRemove(cx - 1);
+                    cx -= 1;
                 }
             },
             else => {
-                try data.items[ro + cy].insert(co + cx, ch);
+                try data.items[cy].insert(cx, ch);
                 cx += 1;
             },
         }
@@ -233,6 +190,22 @@ const Editor = struct {
             mode = Mode.Normal;
         } else {
             try cmd.append(ch);
+        }
+    }
+
+    fn scroll() void {
+        if (get_cx() < coloff) {
+            coloff = cx;
+        }
+        if (get_cx() >= cols + coloff) {
+            coloff = get_cx() - cols + 1;
+        }
+
+        if (cy < rowoff) {
+            rowoff = cy;
+        }
+        if (cy >= rows + rowoff) {
+            rowoff = cy - rows + 1;
         }
     }
 
